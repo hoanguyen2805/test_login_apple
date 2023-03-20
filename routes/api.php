f6c84b1dcce388bdcfc2c2291069dfd804e20ff3
Hòa <53246667+hoanguyen2805@users.noreply.github.com>
2023-03-20 14:39:54 +0700

    2023-03-20 14:39

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::group(['prefix' => 'v1', 'as' => 'v1.'], function () {
+    Route::group(['prefix' => 'login'], function () {
+        Route::get('/', [LoginController::class, 'login'])->name('api_login');
+    });
 });
